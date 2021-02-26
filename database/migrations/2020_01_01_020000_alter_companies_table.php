7ae81a00e50c0da32e67d817f3d8ee66974f00bb
Hermann D. Schimpf <hschimpf@hds-solutinons.net>
2021-02-26 18:04:24 -0300

    Alter Companies table, add Company.logo

---
+++ database/migrations/2020_01_01_020000_alter_companies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use HDSSolutions\Finpar\Blueprints\BaseBlueprint as Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCompaniesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        // get schema builder
+        $schema = DB::getSchemaBuilder();
+
+        // replace blueprint
+        $schema->blueprintResolver(fn($table, $callback) => new Blueprint($table, $callback));
+
+        // alter table
+        $schema->table('companies', function(Blueprint $table) {
+            $table->foreignTo('File', 'logo_id')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        // get schema builder
+        $schema = DB::getSchemaBuilder();
+
+        // replace blueprint
+        $schema->blueprintResolver(fn($table, $callback) => new Blueprint($table, $callback));
+
+        // alter table
+        $schema->table('companies', function(Blueprint $table) {
+            $table->dropForeignTo('File', 'logo_id');
+        });
+    }
+}
