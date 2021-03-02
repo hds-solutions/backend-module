9024e32e8c08374967b2dcd65238781f8571ae21
Hermann D. Schimpf <hschimpf@hds-solutinons.net>
2021-03-02 01:46:30 -0300

    Confirm modal styling

--- resources/views/layouts/modal-confirmation.blade.php
+++ resources/views/layouts/modal-confirmation.blade.php
@@ -1,18 +1,18 @@
 <div class="modal fade" id="confirm-modal" tabindex="-1" role="dialog" aria-labelledby="confirm-modalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirm-modalLabel">Confirmación</h5>
+            <div class="modal-header bg-warning text-dark">
+                <h5 class="modal-title" id="confirm-modalLabel">Confirmation</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p class="m-0">...</p>
+                <p class="m-0">Confirm action?</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" id="accept">Aceptar</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-secondary" id="accept">Aceptar</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
             </div>
         </div>
     </div>
