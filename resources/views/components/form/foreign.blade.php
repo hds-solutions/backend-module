bd6f51e10416b1d2f7933b9ad81b0ba07f0dac2a
Hermann D. Schimpf <hschimpf@hds-solutinons.net>
2021-03-17 02:25:16 -0300

    FilteredBy enabled on master field too

--- resources/views/components/form/foreign.blade.php
+++ resources/views/components/form/foreign.blade.php
@@ -1,7 +1,7 @@
 @if ($secondary)
 
     <div class="col-12 col-md-9 mt-2 mt-xl-0 offset-md-3 offset-xl-0 col-xl-3">
-        <select name="{{ $name }}"
+        <select name="{{ $name }}" data-live-search="true" @if ($required) required @endif
             @if ($filteredBy) data-filtered-by="{{ $filteredBy }}" data-filtered-using="{{ $filteredUsing }}" @endif
             data-none-selected-text="{{ $placeholder }}"
 
@@ -23,7 +23,9 @@
             <option value="{{ $value->id }}"
                 @if ($filteredBy) data-{{ $filteredUsing }}="{{ $value->{"{$filteredUsing}_id"} }}" @endif
                 @if (isset($resource) && !old($name) && $resource->$field == $value->id ||
-                    old($name) == $value->id) selected @endif>{{ $value->name }}</option>
+                    old($name) == $value->id ||
+                    $request && request($request) == $value->id) selected @endif
+                    @foreach ($append as $appended) data-{{ $appended }}="{{ $value->{$appended.'_id'} }}" @endforeach>{{ $value->name }}</option>
             @endforeach
 
             @if ($foreign)
@@ -42,6 +44,7 @@
     <div class="@if ($slot->isEmpty()) col-11 col-md-8 col-lg-6 col-xl-4 @else col-12 col-md-9 col-xl-3 @endif">
 
         <select name="{{ $name }}" data-live-search="true" @if ($required) required @endif
+            @if ($filteredBy) data-filtered-by="{{ $filteredBy }}" data-filtered-using="{{ $filteredUsing }}" @endif
             data-none-selected-text="{{ $placeholder }}"
 
             value="{{ isset($resource) && !old($name) ? $resource->$field : old($name) }}"
@@ -60,6 +63,7 @@
 
             @foreach ($values as $value)
             <option value="{{ $value->id }}"
+                @if ($filteredBy) data-{{ $filteredUsing }}="{{ $value->{"{$filteredUsing}_id"} }}" @endif
                 @if (isset($resource) && !old($name) && $resource->$field == $value->id ||
                     old($name) == $value->id ||
                     $request && request($request) == $value->id) selected @endif
@@ -68,6 +72,7 @@
 
             @if ($foreign)
             <option value="add::new"
+                @if ($filteredBy) data-{{ $filteredUsing }}="*" @endif
                 class="text-muted font-italic">{{ $foreignAddLabel ?? 'undefined' }}</option>
             @endif
 
