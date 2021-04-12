3e603d5f53f85a45c7a0a8a2757af9574fcec30d
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-12 15:54:55 -0400

    HasAfterSave trait

--- app/Models/Base/Model.php
+++ app/Models/Base/Model.php
@@ -2,6 +2,7 @@
 
 namespace HDSSolutions\Finpar\Models\Base;
 
+use HDSSolutions\Finpar\Traits\HasAfterSave;
 use HDSSolutions\Finpar\Traits\HasValidationRules;
 use HDSSolutions\Finpar\Traits\Sortable;
 // use HDSSolutions\Laravel\DynamoDB\Eloquent\SoftDeletes;
@@ -10,6 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 // abstract class Model extends \HDSSolutions\Laravel\DynamoDB\Eloquent\Model {
 abstract class Model extends \Illuminate\Database\Eloquent\Model {
     use HasValidationRules;
+    use HasAfterSave;
     use SoftDeletes;
     use Sortable;
 }
---
+++ app/Traits/HasAfterSave.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HDSSolutions\Finpar\Traits;
+
+use Illuminate\Database\Eloquent\Concerns\HasEvents;
+
+trait HasAfterSave {
+    // use HasEvents;
+
+    public static function bootHasAfterSave() {
+        // capture saved
+        self::saved(function($model) {
+            // execute afterSave
+            $model->afterSave();
+        });
+    }
+
+    /**
+     * Custom validations afterSave
+     */
+    protected function afterSave() {}
+}
