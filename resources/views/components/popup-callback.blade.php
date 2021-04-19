cea57011d3bf5326e1b7fc14d252c002c19663b2
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-19 15:25:24 -0400

    FIX: Skane resource class name for namespace build

--- resources/views/components/popup-callback.blade.php
+++ resources/views/components/popup-callback.blade.php
@@ -1,7 +1,15 @@
-<script type="text/javascript">
-    window.opener.postMessage({
-        namespace: '{{ Str::pluralStudly(strtolower(class_basename(get_class($resource)))) }}',
-        resource: {!! json_encode($resource) !!},
-        saved: {!! $resource->exists !!},
-    }, window.opener.location);
-</script>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>callback</title>
+</head>
+<body>
+    <script type="text/javascript">
+        window.opener.postMessage({
+            namespace: '{{ Str::snake( Str::pluralStudly( class_basename($resource) ) ) }}',
+            resource: {!! json_encode($resource) !!},
+            saved: {{ $resource->exists ? 'true' : 'false' }},
+        }, window.opener.location);
+    </script>
+</body>
+</html>
