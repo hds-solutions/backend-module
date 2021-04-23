d66d05b6b37922b123c3c635029b1598c15e2b79
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-23 05:14:23 -0400

    Append full_name on JSON response

--- app/Traits/ExtendsPerson.php
+++ app/Traits/ExtendsPerson.php
@@ -11,6 +11,10 @@ trait ExtendsPerson {
 
     protected static $identityClass = Person::class;
 
+    public static function bootExtendsPerson() {
+        self::retrieved(fn($model) => $model->appends = $model->appends + [ 'full_name' ]);
+    }
+
     public function getFullNameAttribute():string {
         return $this->business_name ?? ($this->lastname !== null ? $this->lastname.', ' : '').$this->firstname;
     }
