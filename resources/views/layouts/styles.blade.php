a10cc423958fbdf9a95a582022cdd562c5395c1c
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-05-24 17:10:32 -0400

    CSS enhancements

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -3,3 +3,4 @@
 {{-- TinyMCE --}}
 <link rel="stylesheet" type="text/css" href="{{ asset('backend-module/vendor/tinymce/skins/ui/oxide/skin.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('backend-module/vendor/tinymce/skins/ui/oxide/content.min.css') }}">
+@stack('styles')
