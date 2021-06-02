687c07f3d386590be2f6619dcfe970813bd441cb
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-06-02 14:41:14 -0400

    Translations for Role

---
+++ resources/lang/en/role.php
@@ -0,0 +1,15 @@
+<?php return [
+
+    'name'          => [
+        'Name',
+        '_' => 'Name',
+        '?' => 'Name',
+    ],
+
+    'permissions'   => [
+        'Permissions',
+        '_' => 'Permissions',
+        '?' => 'Permissions',
+    ],
+
+];
