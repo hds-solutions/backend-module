5a103eb84d62c8746312af1ea6cff003a7abbb89
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-06-09 17:41:45 -0400

    FIX: Break previews below select

--- resources/views/components/form/backend/image.blade.php
+++ resources/views/components/form/backend/image.blade.php
@@ -53,8 +53,9 @@
     </div>
     @endif
 
+    <div class="w-100"></div>
     <div class="col-11 col-md-8 col-lg-6 col-xl-4 offset-md-3 offset-lg-2 d-flex justify-content-center flex-wrap">
-        <img src="#" id="image_preview" class="m-1 mh-200px rounded" style="display: none;">
+        <img src="#" id="image_preview" class="m-1 mh-150px rounded" style="display: none;">
     </div>
 
 </div>
