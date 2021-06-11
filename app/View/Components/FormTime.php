4bdd9f55a6fef9c3dbddd9b6465cfaa46a8c42f0
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-06-11 15:17:45 -0400

    FIX: Class name

--- app/View/Components/FormTime.php
+++ app/View/Components/FormTime.php
@@ -2,7 +2,7 @@
 
 namespace HDSSolutions\Finpar\View\Components;
 
-class FormDate extends FormText {
+class FormTime extends FormText {
 
     public function render() {
         return fn($data) => view('backend::components.form.backend.time', $data)->render();
