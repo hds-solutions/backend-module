5a8f4d61d2f480ee84e369feb7572196d45fff23
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-06-21 14:26:19 -0400

    Document status translations

---
+++ resources/lang/es/document.php
@@ -0,0 +1,33 @@
+<?php return [
+    'status'    => [
+        'Draft'         => 'Borrador',
+        'InProgress'    => 'En Proceso',
+        'Approved'      => 'Aprobado',
+        'Completed'     => 'Completado',
+        'Rejected'      => 'Rechazado',
+        'Closed'        => 'Cerrado',
+        'Invalid'       => 'Inválido',
+        'Unknown'       => 'Desconocido',
+    ],
+
+    'prepareIt'     => [
+        'Validar',
+        '_' => 'Validar :document',
+        '?' => 'Estas a punto de validar el documento :document',
+    ],
+    'approveIt'     => [
+        'Aprobar',
+        '_' => 'Aprobar :document',
+        '?' => 'Estas a punto de aprobar el documento :document',
+    ],
+    'rejectIt'      => [
+        'Rechazar',
+        '_' => 'Rechazar :document',
+        '?' => 'Estas a punto de rechazar el documento :document',
+    ],
+    'completeIt'    => [
+        'Completar',
+        '_' => 'Completar :document',
+        '?' => 'Estas a punto de completar el documento :document',
+    ],
+];
