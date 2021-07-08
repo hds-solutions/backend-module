2bcc9f835472f4d39c4fdc56ab45c8edd70c7b2c
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-07-08 01:06:49 -0400

    Allow custom ordering and search on DataTables

--- app/DataTables/Base/DataTable.php
+++ app/DataTables/Base/DataTable.php
@@ -2,6 +2,9 @@
 
 namespace HDSSolutions\Finpar\DataTables\Base;
 
+use HDSSolutions\Finpar\Processes\DocumentEngine as Document;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
 use Yajra\DataTables\Html\Editor\Editor;
@@ -16,75 +19,175 @@ abstract class DataTable extends \Yajra\DataTables\Services\DataTable {
     public function __construct(
         protected string $resource,
         protected string $route,
-    ) {}
-
-    /**
-     * Build DataTable class.
-     *
-     * @param mixed $query Results from query() method.
-     * @return \Yajra\DataTables\DataTableAbstract
-     */
-    public final function dataTable($query) {
-        // return datatable class for current eloquent model
-        return datatables()->eloquent($query);
+    ) {
+        // check if request has order specified
+        if (request()->has('order'))
+            // prepend request order columns before default order
+            $this->orderBy = $this->getRequestOrder() + $this->orderBy;
     }
 
-    /**
-     * Get query source of dataTable.
-     *
-     * @param \App\Models\Resource $model
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public final function query() {
+    public final function dataTable($query) {
+        // get datatable class for current eloquent model
+        $datatable = datatables()->eloquent( $this->joins($query) )
+            // redirect order by
+            ->order(fn(Builder $query) => $this->order($query));
+        // foreach registered columns
+        foreach ($this->getColumns() as $column) {
+            // get search method name for current column
+            $searchMethod = $this->searchMethod( $column['name'] );
+            // ignore column if method doesn't exists
+            if (!method_exists($this, $searchMethod)) continue;
+            // register custom search method for column
+            $datatable->filterColumn($column['name'], fn($query, $value) => $this->$searchMethod($query, $value));
+        }
+
+        // return configured datatable
+        return $datatable;
+    }
+
+    public final function query():Builder {
         // return new query for current eloquent model
         $query = (new $this->resource)->newQuery();
-        //
-        if (count($this->with) > 0)
-            //
-            $query->with( $this->with );
-        //
+        // append with to query builder
+        if (count($this->with) > 0) $query->with( $this->with );
+        // return query
         return $query;
     }
 
-    /**
-     * Optional method if you want to use html builder.
-     *
-     * @return \Yajra\DataTables\Html\Builder
-     */
     public final function html() {
-        // return builder with custom columns
-        return $this->builder()
-                    ->setTableId( class_basename($this->resource).'-datatable')
-                    ->autoWidth(false)
-                    ->addTableClass('table-bordered table-sm table-hover')
-                    ->columns($this->getColumns())
-                    ->minifiedAjax( $this->route )
-                    ->searchDelay(150)
-                    // TODO: FIX: order by
-                    // ->orderBy( $this->orderBy )
-                    /*->buttons(
-                        Button::make('create'),
-                        Button::make('export'),
-                        Button::make('print'),
-                        Button::make('reset'),
-                        Button::make('reload'),
-                    )*/;
+        // get builder for current DataTable
+        $builder = $this->builder()
+            ->setTableId( class_basename($this->resource).'-datatable')
+            ->autoWidth(false)
+            ->addTableClass('table-bordered table-sm table-hover')
+            ->columns( $this->getColumns() )
+            ->ajax( $this->route )
+            ->searchDelay(150);
+
+        // load registered columns
+        $columns = collect($this->getColumns());
+        // foreach default orderBy columns
+        foreach ($this->orderBy as $column => $order) {
+            // find column in registered ones
+            $tableColumn = $columns->firstWhere('name', $column);
+            // special case for document_status
+            if ($column === 'document_status' && $tableColumn === null)
+                // find alias column
+                $tableColumn = $columns->firstWhere('name', 'document_status_pretty');
+
+            // ignore if column isn't registered
+            if ($tableColumn === null) continue;
+
+            // add column as default order
+            $builder->orderBy($columns->search($tableColumn), $order);
+        }
+
+        // return configured builder
+        return $builder;
     }
 
-    /**
-     * Get columns.
-     *
-     * @return array
-     */
     protected abstract function getColumns();
 
-    /**
-     * Get filename for export.
-     *
-     * @return string
-     */
-    protected final function filename() {
-        return class_basename( $this->resource ).'_' . date('YmdHis');
+    protected function joins(Builder $query):Builder { return $query; }
+
+    protected final function filename() { return class_basename( $this->resource ).'_' . date('YmdHis'); }
+
+    protected final function orderDocumentStatus(Builder $query, string $order):Builder {
+        // build a raw ORDER BY query
+        $orderByRaw = '';
+        // invert order, ASC goes from Draft to Completed
+        $order = $order == 'asc' ? 'desc' : 'asc';
+        // append Document.statuses with index as order value
+        foreach (Document::STATUSES as $idx => $status)
+            //
+            $orderByRaw .= "CASE WHEN document_status = '$status' THEN $idx END $order, ";
+
+        // return query with custom order
+        return $query->orderByRaw( rtrim($orderByRaw, ', ') );
+    }
+
+    protected final function orderDocumentStatusPretty(Builder $query, string $order):Builder {
+        // alias to document_status order
+        return $this->orderDocumentStatus($query, $order);
+    }
+
+    protected final function searchDocumentStatus(Builder $query, string $value):Builder {
+        $matches = [];
+        // check if value matches some document status string
+        foreach (Document::STATUSES as $status)
+            // check if status code or status translation matches
+            if ( stripos($status, $value) !== false || stripos(Document::__($status), $value) !== false )
+                // add status to matches
+                $matches[] = $status;
+
+        // filter query with matched statuses
+        return count($matches) ? $query->whereIn('document_status', $matches) : $query;
+    }
+
+    protected final function searchDocumentStatusPretty(Builder $query, string $value):Builder {
+        // alias to document_status search
+        return $this->searchDocumentStatus($query, $value);
+    }
+
+    private function getRequestOrder():array {
+        // get registered columns
+        $columns = $this->getColumns();
+        $newOrder = [];
+
+        // parse request[order] values
+        foreach (request('order') as $order) {
+            // check if column is on default list
+            if (array_key_exists(($column = $columns[ $order['column'] ])['name'], $this->orderBy))
+                // remove from array
+                unset($this->orderBy[ $column['name'] ]);
+
+            // special case for document_status
+            if (in_array($column['name'], [ 'document_status', 'document_status_pretty' ])) {
+                // remove from array
+                unset($this->orderBy[ 'document_status' ]);
+                unset($this->orderBy[ 'document_status_pretty' ]);
+                // override colum name
+                $column['name'] = 'document_status';
+            }
+
+            // add column with order direction
+            $newOrder[ $column['name'] ] = $order['dir'];
+        }
+
+        // return new order
+        return $newOrder;
+    }
+
+    private function order(Builder $query):Builder {
+        // foreach selected columns to order by
+        foreach ($this->orderBy as $column => $order) {
+            // build method name
+            $orderMethod = $this->orderMethod( $order->column['name'] ?? $column );
+            // check if method exists
+            if (method_exists($this, $orderMethod))
+                // execure custom method and return
+                $query = $this->$orderMethod($query, $order->order ?? $order);
+            else
+                // order by column name
+                $query->orderBy($order->column['name'] ?? $column, $order->order ?? $order);
+        }
+        // return query with orderBy's
+        return $query;
+    }
+
+    private function columnMethod(string $columnName, string $type):string {
+        // return posible method name for column
+        return $type.Str::studly( str_replace('.', '_', $columnName) );
+    }
+
+    private function orderMethod(string $columnName):string {
+        // return method name for ordering
+        return $this->columnMethod($columnName, 'order');
+    }
+
+    private function searchMethod(string $columnName):string {
+        // return method name for ordering
+        return $this->columnMethod($columnName, 'search');
     }
 
 }
--- resources/assets/js/datatables.js
+++ resources/assets/js/datatables.js
@@ -14,99 +14,96 @@ import { reduce, parse } from './utils/utilities';
 let assetBasePath = document.querySelector('meta[name="assets-path"]').content ?? '';
 function asset(url) { return assetBasePath + url; }
 
-$(_ => {
-    document.querySelectorAll('table[id$=-datatable]').forEach(table => {
-        // check if datatables config exists
-        if (window.datatables === undefined) return;
-        // get datatable configuration
-        let config = window.datatables[table.id] ?? null;
-        // check if null
-        if (!config) return;
-        // get container for current table
-        let container = new Container(table.nextElementSibling, config.ajax.url);
-        // configure columns
-        config.columns.map(column => {
-            switch (true) {
-                case column.name == 'actions':
-                    // set class name
-                    column.className = 'align-middle w-150px';
-                    column.sortable = false;
-                    column.searchable = false;
-                    // render actions using container
-                    column.render = (data, type, row, meta) => {
-                        return container.render(row);
-                    }
-                    break;
-                case column.render !== undefined:
-                    // ignore if already a function
-                    if (typeof column.render === 'function') return;
-                    // get render type
-                    let type = column.render.split(':');
-                    //
-                    type = [ type.shift(), type.join(':') ];
-                    //
-                    switch (type.shift()) {
-                        case 'image':
-                            // set size
-                            column.className = 'text-center align-middle w-200px';
-                            // get column
-                            let col = type.shift();
-                            // render image
-                            column.render = (data, type, row, meta) => {
-                                return '<td><img src="' + (byString(row, col) ?? asset('backend-module/assets/images/default.jpg')) + '" class="mh-75px"></td>';
-                            }
-                            break;
-                        case 'boolean':
-                            column.render = (data, type, row, meta) => data ? 'True' : 'False';
-                            break;
-                        case 'datetime':
-                            // get datetime configuration
-                            let datetime_config = type.shift().split(';'),
-                                field = datetime_config.shift(),
-                                format = datetime_config.shift() || 'F j, Y H:i';
-                            column.render = (data, type, row, meta) => (new Date(data)).format( format );
-                            break;
-                        case 'concat':
-                            // split fields from separator
-                            let concat_config = type.shift().split(';'),
-                                fields = concat_config.shift(),
-                                separator = concat_config.shift() || ', ';
-                            column.render = (data, type, row, meta) => {
-                                // foreach fields to concat
-                                let str = '';
-                                fields.split(',').forEach(field => str += ((str.length ? separator : '') + row[field]) );
-                                return str;
-                            };
-                            break;
-                        case 'view':
-                            column.render = (d, t, row, m) => {
-                                // parse view with object data
-                                return parse(column.data, reduce(row, type[0]));
-                            };
-                            break;
-                    }
-                    break;
-                default:
-                    column.render = (data, type, row, meta) => {
-                        return byString(row, column.data) ?? '--';
-                    }
-            }
-            return column;
-        });
-        // capture draw callback
-        config.drawCallback = e => {
-            // register events
-            container.events();
+document.querySelectorAll('table[id$=-datatable]').forEach(table => {
+    // check if datatables config exists
+    if (window.datatables === undefined) return;
+    // get datatable configuration
+    let config = window.datatables[table.id] ?? null;
+    // check if null
+    if (!config) return;
+    // get container for current table
+    let container = new Container(table.nextElementSibling, config.ajax.url ?? config.ajax);
+    // configure columns
+    config.columns.map(column => {
+        switch (true) {
+            case column.name == 'actions':
+                // set class name
+                column.className = 'align-middle w-150px';
+                column.sortable = false;
+                column.searchable = false;
+                // render actions using container
+                column.render = (data, type, row, meta) => {
+                    return container.render(row);
+                }
+                break;
+            case column.render !== undefined:
+                // ignore if already a function
+                if (typeof column.render === 'function') return;
+                // get render type
+                let type = column.render.split(':');
+                //
+                type = [ type.shift(), type.join(':') ];
+                //
+                switch (type.shift()) {
+                    case 'bold':
+                        column.render = data => '<b>'+data+'</b>';
+                        break;
+                    case 'image':
+                        // set size
+                        column.className = 'text-center align-middle w-200px';
+                        // get column
+                        let col = type.shift();
+                        // render image
+                        column.render = (data, type, row, meta) => {
+                            return '<td><img src="' + (byString(row, col) ?? asset('backend-module/assets/images/default.jpg')) + '" class="mh-75px"></td>';
+                        }
+                        break;
+                    case 'boolean':
+                        column.render = (data, type, row, meta) => data ? 'True' : 'False';
+                        break;
+                    case 'datetime':
+                        // get datetime configuration
+                        let datetime_config = type.shift().split(';'),
+                            field = datetime_config.shift(),
+                            format = datetime_config.shift() || 'F j, Y H:i';
+                        column.render = data => (new Date(data)).format( format );
+                        break;
+                    case 'concat':
+                        // split fields from separator
+                        let concat_config = type.shift().split(';'),
+                            fields = concat_config.shift(),
+                            separator = concat_config.shift() || ', ';
+                        column.render = (data, type, row, meta) => {
+                            // foreach fields to concat
+                            let str = '';
+                            fields.split(',').forEach(field => str += ((str.length ? separator : '') + row[field]) );
+                            return str;
+                        };
+                        break;
+                    case 'view':
+                        column.render = (d, t, row, m) => {
+                            // parse view with object data
+                            return parse(column.data, reduce(row, type[0]));
+                        };
+                        break;
+                }
+                break;
+            default:
+                column.render = (data, type, row, meta) => {
+                    return byString(row, column.data) ?? '--';
+                }
         }
-        // init datatable
-        $(table).DataTable(config);
-        // find selects to apply styling
-        document.querySelectorAll('#'+table.id+'_wrapper select').forEach(select => {
-            // set class
-            select.classList.add('selectpicker');
-            // init select picker
-            $(select).selectpicker();
-        });
+        return column;
+    });
+    // capture draw callback to register events
+    config.drawCallback = e => container.events();
+    // init datatable
+    $(table).DataTable(config);
+    // find selects to apply styling
+    document.querySelectorAll('#'+table.id+'_wrapper select').forEach(select => {
+        // set class
+        select.classList.add('selectpicker');
+        // init select picker
+        $(select).selectpicker();
     });
 });
-
