79677b26ff6f2f8dfd658cc29a62f206a0d2088d
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-07-20 11:43:37 -0400

    Updted confirmation modal, better looking

--- resources/views/layouts/modals/confirmation.blade.php
+++ resources/views/layouts/modals/confirmation.blade.php
@@ -1,17 +1,17 @@
-<div class="modal fade" id="confirm-modal" tabindex="-1" role="dialog" aria-labelledby="confirm-modalLabel" aria-hidden="true">
+<div class="modal fade" id="confirm-modal" tabindex="-1" role="dialog" aria-labelledby="confirm-modal-label" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header bg-warning text-dark">
-                <h5 class="modal-title" id="confirm-modalLabel">Confirmation</h5>
+            <div class="modal-header bg-secondary text-white">
+                <h5 class="modal-title" id="confirm-modal-label">Confirmation</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p class="m-0">Confirm action?</p>
+                <p class="m-0 text-dark">Confirm action?</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" id="accept">Aceptar</button>
+                <button type="button" class="btn btn-outline-primary btn-hover-danger" id="accept">Aceptar</button>
                 <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
             </div>
         </div>
