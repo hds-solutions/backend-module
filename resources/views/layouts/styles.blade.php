382dbf652695f75b917830ecd1a7bfe2a203c8c7
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-07-20 11:45:03 -0400

    CSS assets re-organized

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,6 +1,6 @@
-{{-- Local styles --}}
-<link rel="stylesheet" type="text/css" href="{{ asset(mix('backend-module/assets/css/app.css')) }}">
 {{-- TinyMCE --}}
 <link rel="stylesheet" type="text/css" href="{{ asset('backend-module/vendor/tinymce/skins/ui/oxide/skin.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('backend-module/vendor/tinymce/skins/ui/oxide/content.min.css') }}">
+{{-- Local styles --}}
+<link rel="stylesheet" type="text/css" href="{{ asset(mix('backend-module/assets/css/app.css')) }}">
 @stack('styles')
