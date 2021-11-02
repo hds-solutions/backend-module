62fa11535f6822491059e71760a2dd8fa75aff90
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-11-02 14:28:52 -0300

    Application js library updates
    + Added Application.init() method to execute custom fn after ready
    + Added new function ready() used by Application

--- resources/assets/js/resources/Application.js
+++ resources/assets/js/resources/Application.js
@@ -1,8 +1,26 @@
+require('../utils/ready');
+
 class Application {
 
     constructor() {
         this._classes = {};
         this._instances = {};
+        this._callbacks = [];
+        this.inited = false;
+    }
+
+    init(fn) {
+        // check if already inited
+        if (this.inited) fn();
+        // register callback
+        this._callbacks.push( fn );
+    }
+
+    _doInit() {
+        // execute registered callbacks
+        this._callbacks.forEach(callback => callback());
+        // empty list
+        this._callbacks.empty();
     }
 
     register(name, _class) {
@@ -27,5 +45,7 @@ class Application {
 }
 
 window.Application = window.Application || new Application;
+
+ready(ready => window.Application._doInit());
 
 export default window.Application;
