c22c285750314673f3c9bb359b3fff84f26d1b74
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-11-02 14:34:57 -0300

    Allow to customize text class and prepend class

--- resources/views/components/form/input-group.blade.php
+++ resources/views/components/form/input-group.blade.php
@@ -3,8 +3,8 @@
     'col'   => !str_contains($attributes->get('class'), 'col'),
 ])->except('prepend') }}>
     @if ($attributes->get('prepend', false))
-    <div class="input-group-prepend">
-        <div class="input-group-text">{{ $attributes->get('prepend') }}</div>
+    <div class="input-group-prepend {{ $attributes->get('prepend-class') }}">
+        <div class="input-group-text {{ $attributes->get('text-class') }}">{{ $attributes->get('prepend') }}</div>
     </div>
     @endif
 
